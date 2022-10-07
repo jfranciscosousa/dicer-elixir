7b45c6cb7da13b27f9cf31b0a68469bff31120b7
Francisco Sousa <francisco.sousa@hey.com>
2022-10-07 16:13:09 +0100

    Peach it up

--- lib/dicer_bot/command_registry.ex
+++ lib/dicer_bot/command_registry.ex
@@ -1,13 +1,6 @@
 defmodule DicerBot.CommandRegistry do
-  def call(event) do
-    clear_global_application_commands()
-    Enum.each(event.guilds, &register_commands/1)
-  end
-
-  defp register_commands(guild) do
-    clear_guild_application_commands(guild.id)
-
-    Nostrum.Api.create_global_application_command(%{
+  @commands [
+    %{
       name: "roll",
       description: "Roll dice!",
       options: [
@@ -18,18 +11,28 @@ defmodule DicerBot.CommandRegistry do
           required: true
         }
       ]
-    })
-
-    Nostrum.Api.create_global_application_command(%{
+    },
+    %{
       name: "roll_stats",
       description: "Roll stats for a your character!",
       options: []
-    })
+    }
+  ]
+
+  def call(event) do
+    clear_global_application_commands()
+    Enum.each(event.guilds, &register_commands/1)
+  end
+
+  defp register_commands(guild) do
+    clear_guild_application_commands(guild.id)
+
+    peach(@commands, &Nostrum.Api.create_global_application_command/1)
   end
 
   defp clear_global_application_commands() do
     with {:ok, commands} <- Nostrum.Api.get_global_application_commands(),
-         Enum.each(commands, fn command ->
+         peach(commands, fn command ->
            Nostrum.Api.delete_global_application_command(command.id)
          end) do
       {:ok}
@@ -41,12 +44,17 @@ defmodule DicerBot.CommandRegistry do
   defp clear_guild_application_commands(guild_id) do
     with {:ok, commands} <-
            Nostrum.Api.get_guild_application_commands(guild_id),
-         Enum.each(commands, fn command ->
+         peach(commands, fn command ->
            Nostrum.Api.delete_guild_application_command(guild_id, command.id)
          end) do
       {:ok}
     else
       error -> throw(error)
     end
+  end
+
+  def peach(collection, func) do
+    collection
+    |> Enum.each(&Task.async(fn -> func.(&1) end))
   end
 end
